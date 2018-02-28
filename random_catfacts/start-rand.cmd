bc06e7f70709b3e25091b40cb8d189264c7357de
Boyer <wandax.boyer@intel.com>
2018-02-28 12:19:05 -0800

    Added files from first part of project, which used webhook url to send cat facts randomly to members of a channel

---
+++ random_catfacts/start-rand.cmd
@@ -0,0 +1,3 @@
+PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%TEMP%\StartupLog.txt" 2>&1
+PowerShell C:\Users\wboyerx\Desktop\start-rand.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
+
---
+++ random_catfacts/start-rand.ps1
@@ -0,0 +1,11 @@
+﻿# start-rand.ps1
+$python="C:\Users\wboyerx\AppData\Local\Programs\Python\Python36\python.exe"
+$MyPythonScript="C:\Users\wboyerx\Desktop\cat_facts.py"
+
+while ($true) {
+ $rnd = Get-Random -Minimum 360 -Maximum 2880
+
+ Start-Sleep -Seconds $rnd
+
+ & $python $MyPythonScript *>&1 >> "C:\Users\wboyerx\Desktop\cat_facts.log"
+}
