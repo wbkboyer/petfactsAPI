c42f595c30b2f570988c423a322152dc595df367
wandaboyer <wbkboyer@gmail.com>
2018-02-27 23:00:39 -0800

    updated schema

--- petfacts_schema.sql
+++ petfacts_schema.sql
@@ -7,3 +7,14 @@ id integer PRIMARY KEY  AUTOINCREMENT  NOT NULL,
 fact text  NOT NULL,
 last_shown  datetime
 );
+CREATE TABLE known_users(
+id integer primary key  not null,
+username char(25)  not null,
+latest_query datetime
+, last_received_fact datetime);
+CREATE TABLE logs(
+id integer primary key not null,
+requester char(25) not null,
+response_code integer,
+log_text text
+);
